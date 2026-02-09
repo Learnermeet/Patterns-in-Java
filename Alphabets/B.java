aaaebfb147ab23af340adac4f284a7156e0d4169
Learnermeet <parmarmeet603@gmail.com>
2026-02-09 18:52:01 +0530

    b

--- Alphabets/B.java
+++ Alphabets/B.java
@@ -3,14 +3,11 @@ import java.util.Scanner;
 public class B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter n: ");
         int n = sc.nextInt();
-
         int mid = (n / 2) + 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-
                 if (j == 1 ||                 // left vertical
                     i == 1 ||                 // top
                     i == mid ||               // middle
