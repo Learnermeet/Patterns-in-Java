f4918494455873cf17a551db4acd7f2b9dc8e46e
Learnermeet <parmarmeet603@gmail.com>
2026-02-09 13:38:53 +0530

    A

--- Alphabets/A.java
+++ Alphabets/A.java
@@ -8,10 +8,13 @@ public class A {
         int mid = (n / 2) + 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= 2 * n; j++) {
-                if (j == n - i + 1 || j == n + i - 1) {
+                if (j == n - i + 1 // left diagonal
+                    || j == n + i - 1) { // right diagonal
                     System.out.print("*");
                 }
-                else if (i == mid && j > n - i + 1 && j < n + i - 1) {
+                else if (i == mid // middle row
+                     && j > n - i + 1 // after left diagonal
+                     && j < n + i - 1) { // before right diagonal
                     System.out.print("*");
                 }
                 else {
